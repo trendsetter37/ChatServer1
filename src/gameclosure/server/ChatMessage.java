596d1e89bcbded65460c05d232903f2fe180145b
trendsetter37 <javissullivan@gmail.com>
2013-12-04 15:58:31 -0500

    first Commit

---
+++ src/gameclosure/server/ChatMessage.java
@@ -0,0 +1,22 @@
+package gameclosure.server;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author js0044
+ */
+public class ChatMessage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/gameclosure/server/Server.java
@@ -0,0 +1,15 @@
+package gameclosure.server;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author js0044
+ */
+public class Server {
+    
+}
